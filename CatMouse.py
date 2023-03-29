fa5d6fb47d63c8f9551ccc5983119355e3ec3ee1
Ali Tamer Ünal <atu@icrontech.com>
2023-03-29 08:26:08 +0300

    Added CatMouse.py

---
+++ CatMouse.py
@@ -0,0 +1,71 @@
+import sys, pygame
+
+pygame.init()
+
+class House:
+    def __init__(self):
+        self._animals = []
+        #self._animals.append(Cat(1, 1))
+        self._animals.append(Mouse(1, 1))
+        self._animals.append(Mouse(1, 1))
+
+    def Play(self):
+        while 1:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT: 
+                    self.quit()
+
+            self.timeTick()
+            self._clock.tick()
+
+    def timeTick(self):
+        for a in self._animals:
+            a.move()
+
+        animal_pairs = [(a, b) for idx, a in enumerate(self._animals) for b in self._animals[idx + 1:]]
+
+        caught_animals = []
+        for (a, b) in animal_pairs:
+            if a.check_if_you_caught(b):
+                caught_animals.append(b)
+
+        for a in caught_animals:
+            a.die();        
+
+class Animal:
+    def __init__(self, x, y):
+        self._x = x
+        self._y = y
+
+    def move(self):
+        pass
+
+    def are_you_a_target_at_location(self, x, y):
+        return False
+    
+    def check_if_you_caught(self, a):
+        return False
+
+
+class Mouse(Animal):
+    def __init__(self, x, y):
+         super().__init__(x, y)
+         
+
+    def die(self):
+        pass
+
+    def are_you_a_target_at_location(self, x, y):
+        return self._x == x and self._y == y
+
+
+class Cat(Animal):
+    def __init__(self, x, y):
+         super().__init__(x, y)
+    
+    def check_if_you_caught(self, a):
+        return a.are_you_a_target_at_location(self._x, self._y)
+
+
+h = House()
+h.Play()
