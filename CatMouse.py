fe8e17ced89fe8f37947a365ed85c9026f2c3c5d
asinkilec <nnisanurcelik@gmail.com>
2023-04-03 19:20:10 +0300

    Update CatMouse.py

--- CatMouse.py
+++ CatMouse.py
@@ -4,9 +4,15 @@ pygame.init()
 
 class House:
     def __init__(self):
+<<<<<<< Updated upstream
         self._animals = []
         self._animals.append(Cat(1, 1))
         self._animals.append(Mouse(1, 1))
+=======
+        self._animals = [] #constructer
+        self._animals.append(Cat(1, 1)) #Cat classinda tahmini 3 parameter var. additionally self. MIDTERMDE BOLE BISI SORCAK
+        self._animals.append(Mouse(1, 1)) #polymorphismin olmadigi tek yer bu uc satir. cat mi mouse mu soylememiz lazim
+>>>>>>> Stashed changes
         self._animals.append(Mouse(1, 1))
 
     def Play(self):
@@ -15,8 +21,13 @@ class House:
                 if event.type == pygame.QUIT: 
                     self.quit()
 
+<<<<<<< Updated upstream
             self._timeTick()
             self._clock.tick()
+=======
+            self._timeTick() #timetick de clock da oyun icinde cagirilan fonksiyonlar
+            self._clock.tick() #ondan dolayi ikisi de private variable, DATA HIDING!!
+>>>>>>> Stashed changes
 
     def _timeTick(self):
         for a in self._animals:
@@ -27,7 +38,7 @@ class House:
         caught_animals = []
         for (a, b) in animal_pairs:
             if a.check_if_you_caught(b):
-                caught_animals.append(b)
+                caught_animals.append(b) #polymorphism. objectin sadece animal oldugunu bilmemiz yeterli.
 
         for a in caught_animals:
             a.die();        
@@ -37,7 +48,10 @@ class Animal:
         self._x = x
         self._y = y
 
-    def move(self):
+    def move(self): #_move degil demek ki disardan cagirabiliyoz
+        pass
+
+    def die(self):
         pass
 
     def die(self):
@@ -52,7 +66,11 @@ class Animal:
 class Mouse(Animal):
     def __init__(self, x, y):
          super().__init__(x, y)
+<<<<<<< Updated upstream
          
+=======
+
+>>>>>>> Stashed changes
     def are_you_a_target_at_location(self, x, y):
         return self._x == x and self._y == y
 
